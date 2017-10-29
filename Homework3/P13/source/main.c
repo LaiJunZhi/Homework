5aff2e53b30a51b9fb8156c3893887ba2dee4104
LaiJunZhi <daleliao1141@gmail.com>
2017-10-29 20:06:20 +0800

    homework3-1

---
+++ Homework3/P13/source/main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+double power(double, int);
+
+void main(void)
+{
+	int k;
+	double ans;
+	
+	printf("計算3.5的次方?請輸入k=");
+	scanf("%d", &k);
+	ans = power(3.5, k);
+	printf("3.5的%d次方=%f\n", k, ans);
+	system("pause");
+}
+
+double power(double X, int n)
+{
+	int i;
+	double powerXn = 1;
+	for (i = 1; i <= n; i++)
+		powerXn = powerXn*X;
+	return powerXn;
+}
