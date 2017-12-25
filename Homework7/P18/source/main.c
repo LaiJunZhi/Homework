5d517b5b099768145b5406b82759fd65c4101f69
LaiJunZhi <daleliao1141@gmail.com>
2017-12-25 20:46:47 +0800

    Homework7

---
+++ Homework7/P18/source/main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	FILE *in, *out;
+	int cnt;
+	char str[100];
+	in = fopen("C://Users/daleliao1141/Desktop/Homework/Homework7/P18/welcome.txt", "r");
+	out = fopen("C://Users/daleliao1141/Desktop/Homework/Homework7/P18/output.txt", "w");
+	while (!feof(in))
+	{
+		cnt = fscanf(in, "%s", str);
+		if (cnt > 0)
+			fprintf(out, "%s\n", str);
+	}
+	fclose(in);
+	fclose(out);
+	system("pause");
+	return 0;
+}
